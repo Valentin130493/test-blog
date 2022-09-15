a27f8f245673c59afd74e49ef150fe855eba19f3
Valentin130493 <gerasimenko130493@gmail.com>
2022-09-15 15:28:58 +0300

    create auth

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const bodyParser = require('body-parser')
+const router = require('./router/index.js')
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+app.use('/', router)
 
 
 app.listen(port, () => {
